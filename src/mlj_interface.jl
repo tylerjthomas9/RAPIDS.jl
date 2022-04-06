747e03b62eed0bc0ea8339ae647bd8c51cd30138
Tyler Thomas <tylerjthomas9@gmail.com>
2022-04-06 00:17:15 -0500

    add MLJ package metadata

--- src/mlj_interface.jl
+++ src/mlj_interface.jl
@@ -24,5 +24,13 @@ include("./cuml/regression.jl")
 const CUML_MODELS = Union{CUML_CLASSIFICATION, 
                             CUML_CLUSTERING, 
                             CUML_REGRESSION}
-
 include("./cuml/serialization.jl")
+
+
+MMI.clean!(model::CUML_MODELS) = ""
+
+# MLJ Package Metadata
+MMI.package_name(::Type{<:CUML_MODELS}) = "RAPIDS"
+MMI.package_uuid(::Type{<:CUML_MODELS}) = "2764e59e-7dd7-4b2d-a28d-ce06411bac13"
+MMI.package_url(::Type{<:XGTypes}) = "https://github.com/tylerjthomas9/RAPIDS.jl"
+MMI.is_pure_julia(::Type{<:XGTypes}) = false
