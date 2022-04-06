cb56e4f1f65e3a716a833990cef6463e560f25c2
Tyler Thomas <tylerjthomas9@gmail.com>
2022-04-06 00:17:30 -0500

    fix package metadata

--- src/mlj_interface.jl
+++ src/mlj_interface.jl
@@ -32,5 +32,5 @@ MMI.clean!(model::CUML_MODELS) = ""
 # MLJ Package Metadata
 MMI.package_name(::Type{<:CUML_MODELS}) = "RAPIDS"
 MMI.package_uuid(::Type{<:CUML_MODELS}) = "2764e59e-7dd7-4b2d-a28d-ce06411bac13"
-MMI.package_url(::Type{<:XGTypes}) = "https://github.com/tylerjthomas9/RAPIDS.jl"
-MMI.is_pure_julia(::Type{<:XGTypes}) = false
+MMI.package_url(::Type{<:CUML_MODELS}) = "https://github.com/tylerjthomas9/RAPIDS.jl"
+MMI.is_pure_julia(::Type{<:CUML_MODELS}) = false
