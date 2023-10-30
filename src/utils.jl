8105a77414a89b4d957ef4e5cc3b206c983da4ec
Tyler Thomas <tyler@saguarocm.com>
2023-10-29 20:42:29 -0700

    rework `set_conda_cuda_version`

--- src/utils.jl
+++ src/utils.jl
@@ -2,29 +2,32 @@
 # List of CUDA versions supported by RAPIDSAI
 const supported_versions = ["11.2", "11.8", "12.0"]
 
-# Create a function to find the closest supported version
 function find_closest_supported_version(major, supported_versions)
-    # Filter versions with the same major number
     major_versions = filter(v -> startswith(v, major), supported_versions)
 
-    # If there are versions with the same major number, choose the maximum one
-    if length(major_versions) > 0
+    if !isempty(major_versions)
         return maximum(major_versions)
-    else
-        # Otherwise, choose the maximum supported version that is less than the system's major version
-        less_than_major = filter(v -> parse(Int, split(v, ".")[1]) < parse(Int, major),
-                                 supported_versions)
-        if length(less_than_major) > 0
-            return maximum(less_than_major)
-        else
-            return nothing  # No compatible version found
-        end
     end
-    return error("No compatible CUDA version found for CUDA $(CUDA.driver_version())")
+    less_than_major = filter(v -> parse(Int, split(v, ".")[1]) < parse(Int, major),
+                             supported_versions)
+
+    return !isempty(less_than_major) ? maximum(less_than_major) : nothing
 end
 
 function set_conda_cuda_version!()
-    major, minor, patch = split("$(CUDA.driver_version())", ".")
+    cuda_version = CUDA.driver_version()
+    major, _, _ = split("$cuda_version", ".")
+    @assert major in ["11", "12"] "CUDA Version $cuda_version is not supported."
     closest_version = find_closest_supported_version(major, supported_versions)
-    return CondaPkg.add("cuda-version"; version="=$closest_version")
+
+    dfile = replace(dirname(pathof(RAPIDS)), "src" => "CondaPkg.toml")
+    cur_deps = CondaPkg.read_deps(; file=dfile)["deps"]
+
+    if haskey(cur_deps, "cuda-version")
+        if cur_deps["cuda-version"] != "=$closest_version"
+            CondaPkg.add("cuda-version"; version="=$closest_version", channel="nvidia")
+        end
+    else
+        CondaPkg.add("cuda-version"; version="=$closest_version", channel="nvidia")
+    end
 end
